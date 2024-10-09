7eccd8ebd4a6a7a9f6bc6aa26bc29ea4ecdb6bcc
norbyYT1300 <norbyYT1300@gmail.com>
2024-10-09 11:48:36 -0500

    Slight refactor and add disclaimer

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Accepts an SPT team and generates a printable visualization of your cards, with 
 See the live version of this app here:
 
 https://norbyyt1300.github.io/ShatterpointVisualizer/
+
+The information presented on this site about Shatterpoint, both literal and graphical, is copyrighted by AMG. This website is not produced, supported, or affiliated with AMG.
--- scripts.js
+++ scripts.js
@@ -40,24 +40,28 @@ function lookUpUnitJSON(unitSPTCode) {
 }
 
 function displayUnit(idx, unitJSON) {
+	
+	var IMAGE_SOURCE_FOLDER = "https://pointbreaksw.com/Images/";
+    //var IMAGE_SOURCE_FOLDER = "images/";
+	
     var unitElement = document.getElementById("unit" + (1 + idx));
     unitElement.textContent = '';
 
     var img = document.createElement("img");
     img.classList = "unitImg";
-    img.src = "https://pointbreaksw.com/Images/" + unitJSON.CARD_BACK;
+    img.src = IMAGE_SOURCE_FOLDER + unitJSON.CARD_BACK;
     unitElement.appendChild(img);
 
     var img2 = document.createElement("img");
     img2.classList = "unitStanceImg";
-    img2.src = "https://pointbreaksw.com/Images/" + unitJSON.STANCE1;
+    img2.src = IMAGE_SOURCE_FOLDER + unitJSON.STANCE1;
     unitElement.appendChild(img2);
 
 
     if (unitJSON.STANCE2 != "") {
         var img3 = document.createElement("img");
         img3.classList = "unitStanceImg";
-        img3.src = "https://pointbreaksw.com/Images/" + unitJSON.STANCE2;
+        img3.src = IMAGE_SOURCE_FOLDER + unitJSON.STANCE2;
         unitElement.appendChild(img3);
     }
 }
